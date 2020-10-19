9264f61d7fcc49f89992c900439dfd8d5c9a77d7
fadiHB <eng.fadi.hb@gmail.com>
2020-10-19 08:53:20 +0300

    add color

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.a').css("background-color","#A411AD");
+    $('.b').css("background-color","#F366FA");
+    $('.c').css("background-color","#F7C10F");
+    $('.d').css("background-color","#28F7CD");
+    $('.e').css("background-color","#DBA360");
+    $('.f').css("background-color","#4652DB");
+    $('.g').css("background-color","#40ACDB");
+    $('.h').css("background-color","#DB56A2");
+    $('.i').css("background-color","#DBC46B");
+});
